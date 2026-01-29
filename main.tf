02d26f2f947342349e13e40891d4bec8c1f2ae2c
SrikanthErgula <srikanthreddybtech17@gmail.com>
2026-01-29 12:37:19 +0530

    terraform_start

---
+++ main.tf
@@ -0,0 +1,7 @@
+module "mine-test"" {
+  source = "../mine-module"
+  vpc_cidr = var.vpc_test_cidr
+project_name = var.project_test_name
+environment = var.environment_test
+vpc_tags = var.vpc_test_tags
+}
---
+++ variable.tf
@@ -0,0 +1,43 @@
+variable "vpc_test_cidr" {
+    default = "10.0.0.0/16"
+}
+# defalut ivvakapothe emi anedhi chudali run chesi
+
+variable "project_test_name" {
+    default = "roboshop"
+}
+
+variable "environment_test"{
+    default = "dev"
+}
+
+
+variable "vpc_test_tags" {
+    default = {
+        Purpose = "VPC-Module-Test"
+        DontDelete = "true"
+    }
+}
+
+# # ikkada variales y ante user ki chanace ivvali replce chesukovadaniki,
+
+# # SUBNETS
+
+# variable "public_subnet_cidrs" {
+#     default= ["10.0.1.0/24","10.0.2.0/24"] # see in notes
+# }
+
+# variable "private_subnet_cidrs" {
+#     default= ["10.0.11.0/24","10.0.12.0/24"]
+# }
+
+# variable "database_subnet_cidrs" {
+#     default= ["10.0.21.0/24","10.0.22.0/24"] 
+# }
+# # so here ip adress must unique ga vundali same ip lu use cheyakudadhu chste error vastundhi
+
+
+# # variable "is_peering_required" {
+# #     type = bool # peer.tf lo conditon ichhan so dhaniki access 
+# #     default = true 
+# # }
